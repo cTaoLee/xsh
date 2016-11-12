f8b50f59441779144cb4756a0475f81993e4bd75
cTaoLee <perry_shelock@163.com>
2016-11-12 21:10:28 +0800

    update

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET search page. */
 router.get('/', function(req, res, next) {
-  res.render('search', { title: '' });
+
+	var ng = require('nodegrass');
+	var iconv = require('iconv-lite');
+
+	var url = "http://www.shuquge.com/modules/article/search.php";
+	 	url = "http://127.0.0.1/php/";
+	var reqheaders = {'Content-Type': 'application/x-www-form-urlencoded'};
+
+
+var a = iconv.encode(req.query.value,'gbk');
+a = new Buffer(a,'binary');
+a = a.toString();
+
+	var data = {searchkey: a};
+	var charset = "gbk";
+	var callback = function(r,status,headers){
+		res.send(r);
+	};
+
+	ng.post(url,callback,reqheaders,data,charset);
+
 });
 
 module.exports = router;
+
+
+// _sendReq
