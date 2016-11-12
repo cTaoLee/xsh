b95d16744f60a2126f89dc6d6734c7edc16f4b15
cTaoLee <perry_shelock@163.com>
2016-11-12 13:15:32 +0800

    增加路由

--- index.js
+++ index.js
@@ -2,29 +2,33 @@ var bodyParser = require("body-parser");
 var express = require('express');
 var jade = require('jade');
 
-// create app
+// create app 创建应用
 var app = express();
 
 
-// set views
+// set views 设置模板
 app.set('view engine','jade');
 app.set('views',__dirname+"/views");
 
 
-// set public dir
+// set public dir 设置根访问文件夹
 app.use(express.static('public'));
 
 
-// http analytical 不写这个无法获取到post数据
+// http analytical post数据解析
 app.use(bodyParser.urlencoded({ extended: false }));  
 
 
+// 路由设置
+var index = require('./routes/index');
+var search = require('./routes/search');
 
-app.get("/",function(req,res){
-	res.render('index');
-});
+app.use("/",index);
+app.use("/search",search);
 
 
+
+// 开始服务器
 app.listen(3000,function(){
 	console.log("server is run in 3000")
 })
